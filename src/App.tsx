04c05925a2d3fc31da114485f2219fdc22ff82de
defycab <love.bellinder@gmail.com>
2024-09-17 01:09:33 +0200

    align button center

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { Toggle } from "./components/Toggle"
 
 function App() {
   return (
-    <> 
+    <>
+    <div className="flex items-center">
       <Toggle />
+    </div>
+
     </>
   )
 }
--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,5 +1,8 @@
+
 export function Toggle() {
-    return <button className="flex items-center bg-teal-500 text-white rounded py-2 px-4">
-        Toggle
+  return (
+    <button className="bg-teal-500 text-white rounded py-2 px-4">
+      Toggle
     </button>
+  )
 }
