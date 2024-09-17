4079db07a760650cb8dd9a9b4ecaa64f688ecb06
defycab <love.bellinder@gmail.com>
2024-09-17 15:53:33 +0200

    make toggle button not move with absolute position

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,20 @@ function App() {
 
   return (
     <>
-      <button
-        onClick={handleClick}
-        className="bg-teal-500 text-white rounded py-2 px-4"
-      >
-        Toggle
-      </button>
-      <h1
-        className={isHidden ? "hidden" : ""}
-        style={{ color: "red", paddingTop: "10px" }}
-      >
-        SECRET MESSAGE!
-      </h1>
+      <div className="container">
+        <button
+          onClick={handleClick}
+          className="bg-teal-500 text-white rounded py-2 px-4"
+        >
+          Toggle
+        </button>
+        <h1
+          className={isHidden ? "hidden" : ""}
+          style={{ color: "red", paddingTop: "10px" }}
+        >
+          SECRET MESSAGE!
+        </h1>
+      </div>
     </>
   )
 }
